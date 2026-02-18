9e437db28b608ffd009ae948f71fd40540f48020
Kleber Vales <klebervales.dev@gmail.com>
2026-02-18 11:32:26 -0300

    feat: initial configuration Spring - Java - Gradle groovy DSL

---
+++ src/main/java/com/ecommerce/financeadministrationsystem/FinanceAdministrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.financeadministrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAdministrationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceAdministrationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/financeadministrationsystem/FinanceAdministrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.financeadministrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAdministrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
