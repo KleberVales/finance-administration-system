ebe30895d1fdad6a9783b4f46825bb100f29c613
Kleber Vales <klebervales.dev@gmail.com>
2026-02-23 10:57:29 -0300

    feat: module monolite modular financial control monolith

--- src/main/java/com/financial/financeadministrationsystem/FinanceAdministrationSystemApplication.java
+++ src/main/java/com/financial/financeadministrationsystem/FinanceAdministrationSystemApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce.financeadministrationsystem;
+package com.financial.financeadministrationsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/financial/financeadministrationsystem/FinanceAdministrationSystemApplicationTests.java
+++ src/test/java/com/financial/financeadministrationsystem/FinanceAdministrationSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ecommerce.financeadministrationsystem;
+package com.financial.financeadministrationsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
